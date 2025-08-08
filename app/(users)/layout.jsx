9a7f9744ab678696a6797ac8b80d77e02e52a03c
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-08 16:42:17 +0500

    added next/font

--- app/(users)/layout.jsx
+++ app/(users)/layout.jsx
@@ -1,9 +1,14 @@
 import "../globals.css";
 import Navigation from "./components/Navigation";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+});
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <div className="max-w-[80%] mx-auto">
           <Navigation />
 
