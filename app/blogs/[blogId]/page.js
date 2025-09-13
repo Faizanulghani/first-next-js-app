a4699ae3ce568069dffdd241f1bd21cda0c438b9
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-13 17:41:47 +0500

    Added Not Found

--- app/blogs/[blogId]/page.js
+++ app/blogs/[blogId]/page.js
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export async function generateMetadata({ params }) {
   let { blogId } = await params;
   return {
@@ -7,7 +9,9 @@ export async function generateMetadata({ params }) {
 
 const BlogIdPage = async ({ params }) => {
   let { blogId } = await params;
-
+  if (isNaN(Number(blogId))) {
+    notFound();
+  }
   return <div>Blog Id: {blogId}</div>;
 };
 
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Not Found</h1>
+      <p className="p-1 text-blue-500 underline">Could not find requested resource</p>
+    </div>
+  )
+}
+
+export default NotFound
